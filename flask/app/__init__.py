160b2b569dd7158ebe9f8035f947f240ab0bfe70
wangzhihao <1185956753@qq.com>
2020-08-06 11:37:47 +0800

    2.Vue通过axios访问flask，并引入bootstrap风格

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from config import Config
+# ??CORS??????
+from flask_cors import CORS
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # 注册 blueprint
+    # ??
+    CORS(app)
+
+    # ??blueprint
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
---
+++ front-end/src/components/Ping.vue
@@ -0,0 +1,39 @@
+<template>
+<!-- 用于测试vue前端的功能是否正常 -->
+  <div class="container">
+      <!-- 测试bootstrap风格按钮 -->
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+  </div>
+</template>
+
+
+<script>
+// 引入axios，用于前后端交互
+import axios from 'axios'
+
+export default {
+  name: 'Ping',
+  data() {
+    return {
+      // 空值，其值需要后端传递
+      msg: ''
+    }
+  },
+  methods: {
+      getMessage() {
+          const path = 'http://127.0.0.1:5000/api/ping'
+          axios.get(path)
+            .then((res) => {
+                this.msg = res.data;
+            })
+            .catch((error) =>{
+                // eslint-disable-next-line
+                console.error(error);
+            })
+      }
+  },
+  created() {
+      this.getMessage()
+  }
+}
+</script>
