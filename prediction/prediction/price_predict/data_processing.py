3ac491c413c32dcbd2add5aa3c063e6bb97b3953
Zhang Lei <942471854@qq.com>
2020-08-14 02:10:59 +0800

    对特征进行编码中

--- prediction/prediction/price_predict/data_processing.py
+++ prediction/prediction/price_predict/data_processing.py
@@ -10,14 +10,20 @@ def data_processing(price_data):
 	:param price_data: 处理后的房价数据
 	:return:
 	"""
+	# 从tuple转化为ndarray
 	price_data = np.array(price_data)
 
+	# 建立data列表
+	data_list = []
 	for data in price_data:
-		data_revise(data)
+		data = data_revise(data)
+
+		if data is not None:
+			data_list.append(data)
 
 	# 转化为dataFrame
-	price_data = pd.DataFrame(price_data,
-							  columns=["id", "name", "unit_price", "total_price", "unit_type", "area", "towards",
+	price_data = pd.DataFrame(data_list,
+							  columns=["id", "name", "unit_price", "total_price", "room", "hall", "area", "towards",
 									   "floor",
 									   "address", "year"])
 
@@ -30,6 +36,8 @@ def data_revise(data):
 	:param data:
 	:return:
 	"""
+	data = data.tolist()
+
 	# 去除floor里面的总楼层信息
 	data[7] = re.compile(r'（[^>]+）', re.S).sub('', data[7])
 
@@ -38,3 +46,25 @@ def data_revise(data):
 
 	# 去除year的"年建"
 	data[9] = data[9].replace("年建", "")
+
+	# 将字符变量转化为数值变量
+	data[2] = int(data[2])
+	data[3] = float(data[3])
+	data[5] = float(data[5])
+	data[9] = int(data[9])
+
+	# 排除年份
+	if data[9] > 2020 or data[9] < 1995:
+		return None
+
+	# 将X室X厅分解
+	rooms = data[4][0]
+	halls = data[4][2]
+	data[4] = rooms
+	data.insert(5, halls)
+
+	# 将字符变量转化为数值变量
+	data[4] = int(data[4])
+	data[5] = int(data[5])
+
+	return data
--- prediction/prediction/price_predict/get_model.py
+++ prediction/prediction/price_predict/get_model.py
@@ -1,2 +1,45 @@
+import keras  # 导入Keras
+from keras.models import Sequential  # 导入序贯模型
+from keras.layers import Dense, Dropout  # 导入全连接层
+from keras.optimizers import SGD  # 导入优化函数
+from keras_preprocessing.text import Tokenizer
+from sklearn.model_selection import train_test_split
+
+
+
+def feature_quantification(x):
+	print(x["towards"].value_counts())
+	x = x["towards"]
+
+	tokenizer = Tokenizer()  # 分词器，处理前1000高频出现的单词
+	tokenizer.fit_on_texts(x)  # 创建单词索引
+	sequences = tokenizer.texts_to_sequences(x)  # 把单词转换为序列
+	one_hot_results = tokenizer.texts_to_matrix(x, mode='binary')  # one_hot编码
+	word_index = tokenizer.word_index  # 单词索引表
+
+
+
+	a = keras.utils.to_categorical(x[3], 10)
+	return a
+
+
 def get_model(price_data):
+	x = price_data[["room", "hall", "area", "towards", "floor", "address", "year"]]
+	y = price_data[["unit_price"]]
+	x = feature_quantification(x)
+
+	x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.3)
+
+	# model = Sequential()
+	# model.add(Dense(512, activation="relu"))
+	# model.add(Dropout(0.2))
+	# model.add(Dense(256, activation="relu"))
+	# model.add(Dropout(0.2))
+	# model.add(Dense(10, activation='softmax'))
+	# model.compile(optimizer=SGD(), loss='categorical_crossentropy', metrics=['accuracy'])
+	#
+	# # 训练模型
+	# model.fit(x=x_train, y=y_train, batch_size=32, epochs=100, verbose=1, callbacks=None, validation_split=0.2,
+	# 		  validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0,
+	# 		  steps_per_epoch=None, validation_steps=None)
 	pass
